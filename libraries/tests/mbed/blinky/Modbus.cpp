aa3812472133d2c992c11675969c80a29c9abd6a
mbains <manjinder.bains@gmail.com>
2016-01-08 17:21:43 -0800

    modbus files

---
+++ libraries/tests/mbed/blinky/Modbus.cpp
@@ -0,0 +1,21 @@
+/* 
+ * File:   Modbus.cpp
+ * Author: mbai
+ * 
+ * Created on January 8, 2016, 5:21 PM
+ */
+
+#include "Modbus.h"
+
+Modbus::Modbus()
+{
+}
+
+Modbus::Modbus(const Modbus& orig)
+{
+}
+
+Modbus::~Modbus()
+{
+}
+
---
+++ libraries/tests/mbed/blinky/Modbus.h
@@ -0,0 +1,21 @@
+/* 
+ * File:   Modbus.h
+ * Author: mbai
+ *
+ * Created on January 8, 2016, 5:21 PM
+ */
+
+#ifndef MODBUS_H
+#define	MODBUS_H
+
+class Modbus {
+public:
+    Modbus();
+    Modbus(const Modbus& orig);
+    virtual ~Modbus();
+private:
+
+};
+
+#endif	/* MODBUS_H */
+
